5924e3f65a9ccf3ac11c4ccad3e73cd87070032e
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-27 22:07:01 +0100

    prova

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,9 @@ var firstImg = $('div.images img.first');
 var movement = $('.active');
 $(rightBtn).click(function () { 
     movement = movement.next();
-    $('.images img.first').next(4).addClass('active');
+    $('.images img.first').next().addClass('active');
+    $('.images img.first').removeClass('active');
+
     // console.log($('div.images img.first').
     console.log('click');
 });
