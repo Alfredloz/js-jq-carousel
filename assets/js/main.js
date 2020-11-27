5b1ef459fe1fcb789f306cc6fafbc73e391d0561
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-27 21:55:54 +0100

    seettaggio esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// script JQUEry nel head. Apertura di document ready.
+$(function(){
+//creare effeto carousel 
+//creazioni variabili
+var rightBtn = $('div.next');
+var firstImg = $('div.images img.first');
+var movement = $('.active');
+$(rightBtn).click(function () { 
+    movement = movement.next();
+    $('.images img.first').next(4).addClass('active');
+    // console.log($('div.images img.first').
+    console.log('click');
+});
+});
